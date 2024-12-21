9cf0f52430a317b36f9ea7ad205e526c7cc873c1
Valerio Bozza <valboz@sa.infn.it>
2024-12-21 17:57:08 +0100

    Add files via upload

--- VBMicrolensing/__init__.py
+++ VBMicrolensing/__init__.py
@@ -1,1 +1,8 @@
 from .VBMicrolensing import VBMicrolensing
+
+import inspect
+import os
+
+_fil_in=inspect.getfile(VBMicrolensing)
+VBMicrolensing.SetESPLtablefile(os.path.join(os.path.join(os.path.dirname(_fil_in), 'data'), 'ESPL.tbl'))
+
