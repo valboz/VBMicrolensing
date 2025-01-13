d512f391a4a8f67b7bed4b774b1bfe7eb33f2ced
Valerio Bozza <valboz@sa.infn.it>
2025-01-13 16:42:56 +0100

    Update VBMicrolensingLibrary.cpp

--- VBMicrolensing/lib/VBMicrolensingLibrary.cpp
+++ VBMicrolensing/lib/VBMicrolensingLibrary.cpp
@@ -1,4 +1,4 @@
-// VBMicrolensing v4.3 (2025)
+// VBMicrolensing v4.2 (2025)
 //
 // This code has been developed by Valerio Bozza (University of Salerno) and collaborators.
 // Check the repository at https://github.com/valboz/VBMicrolensing
