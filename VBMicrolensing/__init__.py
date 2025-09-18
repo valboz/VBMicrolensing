ef200876c1342cb3c288372e842ae989ab87d4e4
Valerio Bozza <valboz@sa.infn.it>
2025-09-18 09:47:14 +0200

    Update __init__.py

--- VBMicrolensing/__init__.py
+++ VBMicrolensing/__init__.py
@@ -7,3 +7,6 @@ _fil_in=inspect.getfile(VBMicrolensing)
 VBMicrolensing.SetESPLtablefile(os.path.join(os.path.join(os.path.dirname(_fil_in), 'data'), 'ESPL.tbl'))
 VBMicrolensing.SetSuntablefile(os.path.join(os.path.join(os.path.dirname(_fil_in), 'data'), 'SunEphemeris.txt'))
 
+from importlib.metadata import version
+
+__version__ = version("VBMicrolensing")
