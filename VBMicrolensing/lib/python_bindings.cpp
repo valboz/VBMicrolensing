b1bb78ce2ba4e667782f76e38e9cbd6030235e1b
Valerio Bozza <valboz@sa.infn.it>
2026-09-23 08:52:02 +0200

    Add files via upload

--- VBMicrolensing/lib/python_bindings.cpp
+++ VBMicrolensing/lib/python_bindings.cpp
@@ -43,18 +43,6 @@ PYBIND11_MODULE(VBMicrolensing, m) {
         "Set to 1 if you want to specify a constant t_{0,par}.");
     vbm.def_readwrite("t0_par", &VBMicrolensing::t0_par,
         "Reference time for parallax t_{0,par}. Only used if t0_par_fixed=1.");
-    vbm.def_readwrite("t0_out", &VBMicrolensing::t0_out,
-        "t0 calculated by t0_from_t0_par.");
-    vbm.def_readwrite("tE_out", &VBMicrolensing::tE_out,
-        "tE calculated by t0_from_t0_par.");
-    vbm.def_readwrite("u0_out", &VBMicrolensing::u0_out,
-        "u0 calculated by t0_from_t0_par.");
-    vbm.def_readwrite("alpha_out", &VBMicrolensing::u0_out,
-        "alpha calculated by t0_from_t0_par.");
-    vbm.def_readwrite("paiN_out", &VBMicrolensing::pai1_out,
-        "paiN calculated by t0_from_t0_par.");
-    vbm.def_readwrite("paiE_out", &VBMicrolensing::pai2_out,
-        "paiE calculated by t0_from_t0_par.");
     vbm.def_readwrite("satellite", &VBMicrolensing::satellite,
         "Specifies the satellite number for the next calculation \
                 (0 for observations from the ground);.");
@@ -1924,9 +1912,11 @@ PYBIND11_MODULE(VBMicrolensing, m) {
             )mydelimiter");
 
     vbm.def("t0_from_t0_par",
-        (double (VBMicrolensing::*)(double, double, double, double, double))
-        & VBMicrolensing::t0_from_t0_par,
-        py::return_value_policy::reference,
+        [](VBMicrolensing& self, double t0, double tE, double u0, double pai1, double pai2) {
+            self.t0_from_t0_par(t0, tE, u0, pai1, pai2);
+            std::vector<double> ret = { self.t0_out, self.tE_out, self.u0_out, self.alpha_out, self.pai1_out, self.pai2_out };
+            return ret;
+        },
         R"mydelimiter(
             Given a model obtained with t0_par different from t0, calculates the equivalent model with t0_par=t0.
 
@@ -1945,7 +1935,7 @@ PYBIND11_MODULE(VBMicrolensing, m) {
 
             Returns
             -------
-            Nothing. Results are stored in the properties t0_out, tE_out, u0_out, alpha_out, paiN_out, paiE_out
+            Parameters for the equivalent model: t0_out, tE_out, u0_out, delta_alpha_out, paiN_out, paiE_out
             )mydelimiter");
 
     // Limb darkening
